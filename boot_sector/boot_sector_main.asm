689ae0486fcae081c57d840cbeca03bd9cf707d9
vitorsilverio <vitor.silverio.rodrigues@gmail.com>
2019-08-13 21:49:39 -0300

    fix

--- boot_sector/boot_sector_main.asm
+++ boot_sector/boot_sector_main.asm
@@ -5,11 +5,13 @@ call print
 
 call print_nl
 
+jmp $
+
 %include "boot_sector_print.asm"
 %include "boot_sector_print_hex.asm"
 
 MESSAGE:
-    db 'SIMPLE OS', 0
+    db 'SIMPLE OS', 0xa, 0xd, 'by vitorsilverio', 0
 
 times 510-($-$$) db 0
 dw 0xaa55
