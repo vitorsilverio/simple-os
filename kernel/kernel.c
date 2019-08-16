385313d35a92c116b521a95f5b8040b1a7648974
Vitor S Rodrigues <vitor.silverio.rodrigues@gmail.com>
2019-08-16 09:07:13 -0300

    add message

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -2,6 +2,7 @@
 
 void main() {
     clear_screen();
-    kprint_at(" SIMPLE OS", 0, 0);
+    kprint_at("SIMPLE OS\0", 0, 0);
+    kprint_at("This is an experimental operational system\0", 0, 1);
 }
 
